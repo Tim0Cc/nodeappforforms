7dc801c06c801b923ac032782ed88defa98e07e1
Tim Cecatka <tim.cecatka@gmail.com>
2021-03-13 20:33:53 +0100

    set up express to get public/index.html on Port 300 when running nodemon index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require ('express');
+// const Datastore = require('nedb');
+
+const app = express();
+app.listen(3000, () => console.log('Listening to Port 3000'));
+app.use(express.static('public'));
+// app.use(express.json({limit: '1mb'});
