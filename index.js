6e34e1d2319b3bbdbc3e9a8e5c39a1610f1cc708
Tim Cecatka <tim.cecatka@gmail.com>
2021-03-17 20:28:26 +0100

    npm install nedb && basic database setup

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require ('express');
-// const Datastore = require('nedb');
+const Datastore = require('nedb');
 
 const app = express();
 app.listen(3000, () => console.log('Listening to Port 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit: '100kb'}));
 
+const db = new Datastore('database.db');
+db.loadDatabase();
+
 app.post('/form-data', (req, res) => {
   console.log(req.body);
+  const data = req.body;
+  db.insert(data);
 
   res.json({
     status: 'success'
