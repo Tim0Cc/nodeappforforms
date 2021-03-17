4b868a1b521db808b73a5c3a8e3804b38de7b9ce
Tim Cecatka <tim.cecatka@gmail.com>
2021-03-17 20:05:12 +0100

    getting the data in the request body

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ const express = require ('express');
 const app = express();
 app.listen(3000, () => console.log('Listening to Port 3000'));
 app.use(express.static('public'));
-// app.use(express.json({limit: '1mb'});
+app.use(express.json({limit: '100kb'}));
+
+app.post('/form-data', (req, res) => {
+  console.log(req.body);
+
+  // res.json({
+  //   status: 'success'
+  // });
+});
